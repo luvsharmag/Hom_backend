fa4667b6d21bc26de16c1ea3c4462cdb3bc60b04
luvsharmag <luvsharmagju31@gmail.com>
2024-10-06 15:27:04 +0530

    fdfd

--- src/server.js
+++ src/server.js
@@ -6,4 +6,5 @@ connectDB();
 const port = process.env.PORT || 3000;
 app.listen(port,()=>{
     console.log(`server is working on port:${process.env.PORT} in ${process.env.NODE_ENV} Mode`);
+    console.log(process.env.PORT);
 });
